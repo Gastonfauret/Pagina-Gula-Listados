b381039477799e9a513fa1da02cf360b295d0a69
unknown <tomicardenas96@gmail.com>
2023-06-20 17:38:24 -0300

    Added data of clients

--- src/components/Data-clients.jsx
+++ src/components/Data-clients.jsx
@@ -1,22 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import "../styles/data-clients.css";
 
 function DataClients() {
-    let [gets, setGets] = useState([]);
+  let [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        fetch("https://649115f02f2c7ee6c2c7b868.mockapi.io/clients")
-        .then(response => response.json())
-        .then(data => {
-            data.map(item => {
-                
-            })
-            })
-        }, []);
+  useEffect(() => {
+    getUsers();
+  }, []);
 
-    return (
-        <>
-        </>
-    )
+  const getUsers = () => {
+    fetch("https://649115f02f2c7ee6c2c7b868.mockapi.io/clients")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  };
 
-    };
+  return (
+    <>
+    {true &&
+      <table className="data-clients-container">
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{ user.lastName }</td>
+              <td>{ user.name }</td>
+              <td>{ user.dni }</td>
+              <td>{ user.address }</td>
+              <td>{ new Date(user.birthdate).toLocaleDateString('en-US')}</td>
+              <td>{ user.phone }</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+  }
+    </>
+  );
+}
 export default DataClients;
